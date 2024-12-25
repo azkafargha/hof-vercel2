e5567818a3a2c7c76ba1e5edf5273da54e78270c
azkafargha <azkafarghaa@gmail.com>
2024-12-25 18:20:28 +0700

    fix ga? gatau ges?

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,16 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = decoded; // Tambahkan user ke req untuk penggunaan selanjutnya
-    next();
-  } catch (error) {
-    res.status(403).json({ message: "Invalid token" });
+  if (token == null) {
+    return res.status(401).json({ error: "unauthenticated" });
   }
-};
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "unauthorized" });
+    }
+    req.user = user;
+
+    next();
+  });
+}
 
 module.exports = verifyJWT;
